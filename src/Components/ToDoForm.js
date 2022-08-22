679f886af02c4a601bb28cfcb971ba9531df8527
Hedieh Heydari <heydari.hedieh2680@gmail.com>
2022-08-22 19:40:41 +0430

    added todos

--- src/Components/ToDoForm.js
+++ src/Components/ToDoForm.js
@@ -1,18 +1,24 @@
 import { useState } from "react";
 
-const ToDoForm = () => {
-    const [todo, setTodo] = useState('')
+const ToDoForm = (props) => {
+    const [input, setInput] = useState('')
     const changeHandler = (e) => {
-        setTodo(e.target.value)
+        setInput(e.target.value)
         console.log(e.target.value)
     }
     const submitHandler = (e) => {
         e.preventDefault();
+        if (!input) {
+            alert('enter todo!')
+            return
+        }
+        props.addTodoHandler(input)
+        setInput("")
     }
     return (
         <>
             <form onSubmit={submitHandler}>
-                <input type='text' value={todo} onChange={changeHandler} />
+                <input type='text' value={input} onChange={changeHandler} />
                 <button type="submit">add</button>
             </form>
         </>
--- src/Components/TodoApp.js
+++ src/Components/TodoApp.js
@@ -5,10 +5,19 @@ import ToDoList from "./TodoList";
 const TodoApp = () => {
     const [todos, setTodos] = useState([]);
 
+
+    const addTodoHandler = (input) => {
+        const newTodo = {
+            id: Math.floor(Math.random() * 1000),
+            text: input,
+            isCompleted: false
+        }
+        setTodos(...todos, newTodo)
+    }
     return (
 
         <div className="container">
-            <ToDoForm />
+            <ToDoForm addTodoHandler={addTodoHandler} />
             <ToDoList />
         </div>
     );
